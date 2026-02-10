cf19c9e3f1679c78fcd00641d3c0657af326d07e
Tajim-Akand <tajimislam605@gmail.com>
2026-02-10 21:47:08 +0600

    Update digit_count.py

--- digit_count.py
+++ digit_count.py
@@ -1,25 +1,25 @@
-n = int(input())
+def digit_count_and_sum(n):
+    if n < 0:
+        n = -n
 
-n = abs(n)
+    count = 0
+    total_sum = 0
 
-count = 0
-
-total = 0
-
-if n == 0:
-
-    count = 1
-
-else:
+    if n == 0:
+        return 1, 0
 
     while n > 0:
-
-        total += n % 10
-
+        digit = n % 10
+        total_sum += digit
         count += 1
-
         n //= 10
 
-print("Digits:", count)
+    return count, total_sum
 
-print("Sum:", total)
+
+num = int(input("Enter an integer: "))
+count, digit_sum = digit_count_and_sum(num)
+
+print("Digit count:", count)
+print("Digit sum:", digit_sum)
+
